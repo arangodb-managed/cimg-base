4eb3e807c614aa9203c1b02d98628d75bf035282
Ewout Prangsma <ewout@prangsma.net>
2021-12-18 15:03:52 +0100

    OAS-3855 | Supporting arm64

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-docker build --file 18.04/Dockerfile -t cimg/base:18.04 .
-docker build --file 20.04/Dockerfile -t cimg/base:20.04 .
+DOCKERIMAGE=arangodboasis/cimg-base:${CIRCLE_TAG:-$CIRCLE_BRANCH-$CIRCLE_SHA1}"}
+
+docker buildx install
+docker build \
+    --platform linux/amd64,linux/arm64 \
+    --push \
+    --file 20.04/Dockerfile
+    -t ${DOCKERIMAGE} .
